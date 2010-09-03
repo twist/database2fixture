1aa9e3e07a49f1c2cb3f52df5b334038248f0b47
twist <bellee@webit.de>
2010-09-03 16:54:56 +0200

    module wrapper

--- lib/database2fixtures.rb
+++ lib/database2fixtures.rb
@@ -1,12 +1,15 @@
 # Database2fixtures
 
 
+module Db2Fixture
 
+def load
 path = "./" # RAILS_ROOT
 # find all models with a database connection
 
 
 models = `grep '< ActiveRecord::Base' #{path}`
 puts models.inspect
+end
 
 
