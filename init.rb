38bad0088c6ca47aa68d76db8c50fd5104980d79
twist <bellee@webit.de>
2010-09-03 16:52:42 +0200

    starting to look for models

--- init.rb
+++ init.rb
@@ -1,1 +1,2 @@
 # Include hook code here
+require 'database2fixture'
--- lib/database2fixtures.rb
+++ lib/database2fixtures.rb
@@ -1,1 +1,12 @@
 # Database2fixtures
+
+
+
+path = "./" # RAILS_ROOT
+# find all models with a database connection
+
+
+models = `grep '< ActiveRecord::Base' #{path}`
+puts models.inspect
+
+
